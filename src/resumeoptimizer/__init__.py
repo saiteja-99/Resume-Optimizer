609f3b6d573c80b5b3930154ec1b580878653078
Sai Teja <Saiteja.edara9999@gmail.com>
2026-09-15 19:30:36 +0200

    Restore package version metadata

--- src/resumeoptimizer/__init__.py
+++ src/resumeoptimizer/__init__.py
@@ -1,3 +1,19 @@
-"""Resume Optimizer package."""
+"""Resume Tailoring and ATS Optimization Toolkit."""
+
+from resumeoptimizer.models import (
+    AnalysisResult,
+    JobDescription,
+    Recommendation,
+    Resume,
+    ScoreBreakdown,
+)
+
+__all__ = [
+    "AnalysisResult",
+    "JobDescription",
+    "Recommendation",
+    "Resume",
+    "ScoreBreakdown",
+]
 
 __version__ = "0.1.0"
